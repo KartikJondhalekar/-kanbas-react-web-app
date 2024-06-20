f0516b5379f6e17df49f687b3c8f60ee007fd398
jondhalekark <jondhalekar.k@northeastern.edu>
2024-06-19 21:05:39 -0400

    Resolved netlify warnings

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./index.css";
-import { useLocation, useParams } from "react-router";
+import { useLocation } from "react-router";
 
 export default function CoursesNavigation() {
     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
